bb5bb8c69fbd5e635fa14e13dfc25984334c2079
Frank Pagan <frank@cocreate.app>
2020-09-23 18:05:18 +0000

    update documentation

--- src/CoCreate-toggle.js
+++ src/CoCreate-toggle.js
@@ -1,36 +1,3 @@
-// let list_data_toogles = document.querySelectorAll('[data-toggle]');
-
-// list_data_toogles.forEach(elem=>{
-    
-//     elem.addEventListener("click",function(e){
-//         let value = elem.dataset['toggle'];
-//         let toggle_closest = elem.dataset['toggle_closest'];
-//         if(typeof(toggle_closest) != 'undefined'){
-//             let list_toggle_closest = elem.closest(toggle_closest);
-//             if(list_toggle_closest)
-//                 if(Array.isArray(list_toggle_closest))
-//                     list_toggle_closest.forEach(el=>{
-//                         ToogleClass(el,value);
-//                     });
-//                 else
-//                     ToogleClass(list_toggle_closest,value);
-//         }else{
-//             ToogleClass(elem,value);
-//         }
-//     });
-    
-// });
-
-// function ToogleClass(elem,clase){
-//     if(elem.classList.contains(clase))
-//         elem.classList.remove(clase)
-//     else
-//         elem.classList.add(clase)
-// }
-
-
-
-
 let list_data_toogles = document.querySelectorAll('[data-toggle]');
 
 list_data_toogles.forEach(elem=>{
@@ -40,17 +7,21 @@ list_data_toogles.forEach(elem=>{
         let target_toggle_element = elem.dataset['toggle_element'] ? elem.dataset['toggle_element'] : false;
         let toggle_closest = elem.dataset['toggle_closest'];
         let elem_change = elem;
-        let array_2 = elem.getAttribute(target).split(' ').map(function(x){return x.trim()});
-        let  toggle_tmp = get_val_toggle_attributes(values,array_2);
-        let tmp_val = get_value(values,toggle_tmp);
         if(typeof(toggle_closest) != 'undefined'){
             elem_change = elem.closest(toggle_closest);
         }
         if(target_toggle_element){
                 document.querySelectorAll(target_toggle_element).forEach(elem_change=>{
-                    change_values(target,toggle_tmp,values,tmp_val,elem,elem_change);
+                    let b_values = values
+                    let array_2 = elem_change.getAttribute(target).split(' ').map(function(x){return x.trim()});
+                    let  toggle_tmp = get_val_toggle_attributes(b_values,array_2);
+                    let tmp_val = get_value(b_values,toggle_tmp);
+                    change_values(target,toggle_tmp,b_values,tmp_val,elem,elem_change);
                 });
         }else{
+            let array_2 = elem.getAttribute(target).split(' ').map(function(x){return x.trim()});
+            let  toggle_tmp = get_val_toggle_attributes(values,array_2);
+            let tmp_val = get_value(values,toggle_tmp);
             if(change_values(target,toggle_tmp,values,tmp_val,elem,elem_change))
                 return
         }
@@ -59,8 +30,8 @@ list_data_toogles.forEach(elem=>{
 
 
 function get_val_toggle_attributes(values,array_2){
-    console.log("First Array ",values)
-    console.log("Segundo Array ",array_2)
+    //console.log("First Array ",values)
+    //console.log("Segundo Array ",array_2)
     var array_first = values;
     var array_second = array_2;
     
@@ -72,16 +43,19 @@ function get_val_toggle_attributes(values,array_2){
     		return false;
     });
     let retorno = (array_intersection.length ) ? array_intersection[0] : '';
-    console.log("retorno ",retorno)
+    //console.log("retorno ",retorno)
+    //console.log("Intercept ",array_intersection);
     return retorno
         
-    //console.log("Intercept ",array_intersection);
+    
     
 }
 
 function change_values(target,toggle_tmp,values,tmp_val,elem,elem_change){
-    console.log("elem_change",elem_change)
+    //console.log("elem_change",elem_change)
+    //console.log("toggle_tmp --- ",toggle_tmp)
     if(target=='class' && toggle_tmp!=''){
+      //  console.log("toggle_tmp")
             elem_change.classList.remove(toggle_tmp)
             if(values.length==1){
                 //elem.dataset['toggle_tmp'] = '';
@@ -89,7 +63,7 @@ function change_values(target,toggle_tmp,values,tmp_val,elem,elem_change){
             }
         }
         if(target=='class' ){
-            console.log("Target ",target)
+        //    console.log("Target ",target)
             //&& tmp_val!=''
             if(tmp_val!='')
                 elem_change.classList.add(tmp_val)
